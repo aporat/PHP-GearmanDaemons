225e7f736d1bc0cfb7e401f2a298244a3ed98d19
Adar Porat <adar.porat@gmail.com>
2013-03-08 19:08:46 -0500

    Update install_dependencies.sh

--- tests/install_dependencies.sh
+++ tests/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo add-apt-repository ppa:gearman-developers/ppa 
-sudo apt-get update
-sudo apt-get install gearman-job-server libgearman-dev gearman-tools
+sudo add-apt-repository ppa:gearman-developers/ppa -y
+sudo apt-get update  -y
+sudo apt-get install gearman-job-server libgearman-dev gearman-tools -y
 sudo apt-get install libevent-dev uuid-dev -y
 
 wget https://launchpad.net/gearmand/1.2/1.1.4/+download/gearmand-1.1.4.tar.gz
