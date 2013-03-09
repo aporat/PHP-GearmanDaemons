25b8f7ec31ef6e9ea7fc1d8931693e0d4fa3bea7
Adar Porat <adar.porat@gmail.com>
2013-03-09 12:56:26 -0500

    Update install_dependencies.sh

--- tests/install_dependencies.sh
+++ tests/install_dependencies.sh
@@ -4,3 +4,7 @@ sudo add-apt-repository ppa:gearman-developers/ppa -y
 sudo apt-get update  -y
 sudo apt-get install gearman-job-server libgearman-dev gearman-tools -y
 sudo apt-get install libevent-dev uuid-dev -y
+
+
+pecl install gearman
+echo "extension=gearman.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
